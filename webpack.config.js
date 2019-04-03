a41c01af2dde332970a7816cb85640ab169c28b3
Okwukwe Ewurum <okwukwe.ewurum@gmail.com>
2019-04-03 18:24:35 +0100

    chore(herokuDeploy): setup heroku deploy :wrench:
    - add server.js
    - add Procfile for heroku
    
    [Delivers #165093362]

--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,43 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
 
 module.exports = (env, argv) => ({
   entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "main.bundle.js",
+    filename: "main.bundle.js"
   },
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: "babel-loader",
+        use: "babel-loader"
       },
       {
         test: /\.(css|scss|sass)$/,
-        use: ["style-loader", "css-loader", "sass-loader"],
+        use: ["style-loader", "css-loader", "sass-loader"]
       },
       {
         test: /\.(png|jpg|gif|woff|woff2|eot|ttf|otf|svg)$/,
         use: [
           {
-            loader: "file-loader",
-          },
-        ],
-      },
-    ],
+            loader: "file-loader"
+          }
+        ]
+      }
+    ]
   },
   resolve: { extensions: ["*", ".js", ".jsx"] },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "public/index.html",
-    }),
+      template: "public/index.html"
+    })
   ],
   devServer: {
     port: 3000,
     contentBase: path.join(__dirname, "public"),
-    historyApiFallback: true,
+    historyApiFallback: true
   },
-  devtool: argv.mode === "development" ? "cheap-eval-source-map" : "source-map",
-});
+  devtool: argv.mode === "development" ? "cheap-eval-source-map" : "source-map"
+})
